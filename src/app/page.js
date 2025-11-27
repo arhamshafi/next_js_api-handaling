6efdb680ec9221d16fa2932af5fceb92f8d99217
arhamshafi <arhambutt2923@gmail.com>
2025-11-27 17:29:56 +0500

    added new files / folders

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <div className="w-full min-h-screen bg-black p-1">
+
+      <h1 className="text-center text-white text-2xl mt-10 tracking-[2px] " > TODO APP </h1>
+
+    </div>
+  );
+}
---
+++ src/data/todo.js
@@ -0,0 +1,6 @@
+export const todo = [
+    { id: 1, text: "CRUD operation practice", compelete: false },
+    { id: 2, text: "next.js  API handling ", compelete: false }
+]
+
+export const gettodo = () => { return todo }
